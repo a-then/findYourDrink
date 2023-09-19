30db4fca8b3b09329d10a4675c6582dae4c4d714
athen <ayoslinthen@gmail.com>
2023-09-18 21:51:53 -0400

    stylistic changes to style.css

--- js/main.js
+++ js/main.js
@@ -11,11 +11,19 @@ function getDrink() {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      const cocktail = data.drinks[0]
+      showCocktailInfo(data)
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+}
+
+function showCocktailInfo(data) {
+  const cocktail = data.drinks[0]
       console.log(cocktail)
       document.querySelector('h2').innerText = cocktail.strDrink
       document.querySelector('#cocktailThumb').src = cocktail.strDrinkThumb
-      document.querySelector('.instructions').innerText = cocktail.strInstructions
+      document.querySelector('#instructions').innerText = cocktail.strInstructions
       
       document.querySelectorAll('li').forEach( currLi => currLi.remove() )
       Object.entries(cocktail).map(([key, val] ) => {
@@ -23,9 +31,4 @@ function getDrink() {
           document.getElementById('ingredients').appendChild( document.createElement('li')).textContent = val  
         }
       })
-
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
 }
