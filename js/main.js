83fade454882842b1d7da9de1ab16e6d69b64e10
athen <ayoslinthen@gmail.com>
2024-04-01 17:21:10 -0400

    Uniform design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
+// TO-DO: add event listener to return keypress
+// TO-DO: add recent search list, maybe to localStorage?
 
 const button = document.querySelector('button')
+const drinkContainer = document.querySelector('#hidden')
+drinkContainer.setAttribute('id', 'hidden')
 
 button.addEventListener('click', getDrink)
 
 function getDrink() {
   const drink = document.querySelector('input').value
-
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
+
       showCocktailInfo(data)
     })
     .catch(err => {
@@ -19,9 +23,12 @@ function getDrink() {
 }
 
 function showCocktailInfo(data) {
+  
+  drinkContainer.removeAttribute('id')
+
   const cocktail = data.drinks[0]
       console.log(cocktail)
-      document.querySelector('h2').innerText = cocktail.strDrink
+      document.querySelector('.drink').innerText = cocktail.strDrink
       document.querySelector('#cocktailThumb').src = cocktail.strDrinkThumb
       document.querySelector('.instructions').innerText = cocktail.strInstructions
       const ingredients = document.querySelector('.ingredients')
@@ -29,6 +36,7 @@ function showCocktailInfo(data) {
       // document.querySelectorAll('li').forEach( currLi => currLi.remove() )
       
       Object.entries(cocktail).map(([key, val] ) => {
+        location.replace(location.href)
 
         if ( key.startsWith('strMeasure') && (val !== null) ) {
           document.querySelector('.measures').appendChild( document.createElement('li')).textContent = val
